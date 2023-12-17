aefdfb5dcfa5ec48e75172030d322600c2e2091d
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-17 16:01:12 +0300

    Проверена работоспособность классов tailwind и их сортировка

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <Button variant={'destructive'}>Click me</Button>
+      <p className='text-2xl text-red-500 lg:text-4xl'>Hello world</p>
     </div>
   );
 }
