8a88f3434c667c4122af8b6496f393f2e108780a
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-17 17:04:01 +0300

    Удален layout.tsx, который использовался в образовательных целях

--- app/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='flex h-full items-center justify-center bg-gray-500'>
-      {children}
-    </div>
-  );
-};
-
-export default DashboardLayout;
