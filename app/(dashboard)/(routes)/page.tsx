0bc6a2cdad971438f9faa5990e6aaffc5f571fa3
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-18 13:07:50 +0300

    Добавление компонента UserButton для тестирования интеграции с сервисом Clerk

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div>
       <Button variant={'destructive'}>Click me</Button>
       <p className='text-2xl text-red-500 lg:text-4xl'>Hello world</p>
+      <UserButton afterSignOutUrl='/' />
     </div>
   );
 }
