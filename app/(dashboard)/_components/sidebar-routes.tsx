dd5305e5d8a35423f5fac72d3d73c008020aa006
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-19 12:58:32 +0300

    Создана переменная GUEST_ROUTES
     - Создана константа `GUEST_ROUTES` за пределами функции компонента `SidebarRoutes`
    
     - Значение `GUEST_ROUTES` присвоено переменной `routes`, так как в дальнейшем будут использоваться разные константы в зависимости от `pathname`

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,23 @@
 'use client';
 
+import { Compass, Layout } from 'lucide-react';
+
+const GUEST_ROUTES = [
+  {
+    icon: Layout,
+    label: 'Dashboard',
+    href: '/',
+  },
+  {
+    icon: Compass,
+    label: 'Browse',
+    href: '/search',
+  },
+];
+
 const SidebarRoutes = () => {
+  const routes = GUEST_ROUTES;
+
   return (
     <div className='flex flex-col'>
       <p>Sidebar Routes</p>
