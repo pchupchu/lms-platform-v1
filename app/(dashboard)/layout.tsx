34c40acdbf085b29e7fda8e0c4b86600912d9e6a
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-19 11:32:32 +0300

    Добавление обертки, которая будет скрывать Sidebar на мобильном разрешении

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,10 @@
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className='h-full'>{children}</div>;
+  return (
+    <div className='h-full'>
+      <div className='fixed z-50 hidden h-full w-56 bg-red-200 md:block'></div>
+      {children}
+    </div>
+  );
 };
 
 export default DashboardLayout;
