46e236137dbb77d5ca6af618ede79b35067e03f3
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-19 11:54:38 +0300

    Исправлена ошибка в стилях для обертки Sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from './_components/sidebar';
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='h-full'>
-      <div className='fixed z-50 hidden h-full w-56 bg-red-200 md:block'>
+      <div className='fixed z-50 hidden h-full w-56 md:flex'>
         <Sidebar />
       </div>
       {children}
