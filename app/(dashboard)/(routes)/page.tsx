1402a71017a7a38fa9617a594b1aac57b022e0c4
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-19 11:32:08 +0300

    Очищен page.tsx от лишних тегов

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -4,8 +4,6 @@ import { UserButton } from '@clerk/nextjs';
 export default function Home() {
   return (
     <div>
-      <Button variant={'destructive'}>Click me</Button>
-      <p className='text-2xl text-red-500 lg:text-4xl'>Hello world</p>
       <UserButton afterSignOutUrl='/' />
     </div>
   );
