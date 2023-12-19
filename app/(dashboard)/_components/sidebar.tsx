4eb7114d2729d01e20c862feb17e99cea833f947
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-19 11:54:58 +0300

    Добавлены стили для Sidebar

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,5 +1,9 @@
 const Sidebar = () => {
-  return <div>This is a Sidebar</div>;
+  return (
+    <div className='flex w-full flex-col overflow-y-auto border-r bg-white shadow-sm'>
+      This is a Sidebar
+    </div>
+  );
 };
 
 export default Sidebar;
