8e2fd0891191f0f2619dbdded713d72dfa32f581
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-20 13:19:11 +0300

    Добавление стилей для тега button и его динамической стилизации

--- app/(dashboard)/_components/sidebar-route.tsx
+++ app/(dashboard)/_components/sidebar-route.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import { LucideIcon } from 'lucide-react';
 import { usePathname, useRouter } from 'next/navigation';
 
@@ -13,13 +14,20 @@ const SidebarRoute = ({ icon: Icon, label, href }: SidebarRouteProps) => {
   const pathname = usePathname();
   const router = useRouter();
 
-  const isActive = href === pathname || pathname?.startsWith(`${href}`);
+  const isActive = href === pathname || pathname?.startsWith(`${href}/`);
   const onClick = () => {
     router.push(href);
   };
 
   return (
-    <button type='button' onClick={onClick}>
+    <button
+      className={cn(
+        'flex items-center gap-x-2 pl-6 text-sm font-medium text-slate-500 transition-all hover:bg-slate-300/20 hover:text-slate-600',
+        isActive &&
+          'bg-sky-200/20 text-sky-700 hover:bg-sky-200/20 hover:text-sky-700',
+      )}
+      type='button'
+      onClick={onClick}>
       {label}
     </button>
   );
