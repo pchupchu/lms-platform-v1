e85e0a27902af042454f986d040a3b66044b2442
Marina Malyarchuk <tempermailfordev@gmail.com>
2023-12-21 12:55:13 +0300

    Добавление иконки в компонент SheetTrigger
    - Импорт иконки `Menu` из библиотеки `lucide-react`
    
    - Добавление стилизации для `SheetTrigger`

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,9 +1,12 @@
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Menu } from 'lucide-react';
 
 const MobileSidebar = () => {
   return (
     <Sheet>
-      <SheetTrigger></SheetTrigger>
+      <SheetTrigger className='pr-4 transition hover:opacity-75 md:hidden'>
+        <Menu />
+      </SheetTrigger>
       <SheetContent></SheetContent>
     </Sheet>
   );
