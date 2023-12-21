b0f37e07f64aa65c15bbd7eecefe03f7455d3121
Marina Malyarchuk <tempermailfordev@gmail.com>
2023-12-21 12:23:01 +0300

    Создание и стилизация обертки для будущего компонента Navbar
    Создание обертки, которая будет сдвигать его вправо на ширину компонента `Sidebar` на разрешении `md` и выше

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import Sidebar from './_components/sidebar';
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='h-full'>
+      <div className='fixed z-50 h-[80px] w-full md:pl-56'></div>
       <div className='fixed z-50 hidden h-full w-56 md:flex'>
         <Sidebar />
       </div>
