80528c027769f33a41ed014e46375dbb31c85471
Marina Malyarchuk <tempermailfordev@gmail.com>
2023-12-21 12:16:35 +0300

    Добавление тега main
    - В компоненте `DashboardLayout` создается тег `main`, который оборачивает `children`
    
    - Ему добавляются стили, чтобы увидеть контент, который спрятан за `Sidebar`

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,7 +6,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <div className='fixed z-50 hidden h-full w-56 md:flex'>
         <Sidebar />
       </div>
-      {children}
+      <main className='h-full md:pl-56'>{children}</main>
     </div>
   );
 };
