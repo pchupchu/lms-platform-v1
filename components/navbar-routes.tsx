27504fc19648c7a81fc3491eecb5480b778fb243
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-22 13:54:52 +0300

    Отображение кнопок в `NavbarRoutes`
    - Условный рендеринг внутри `NavbarRoutes` с использованием тернарного оператора
    
    - Если текущая страница связана с `TeacherMode` или режимом просмотра курса, то отображается кнопка переадресации пользователя на главную страницу
    
    - В противном случае отображается кнопка, которая перенаправляет пользователя в `TeacherMode`
    
    - Для отображения кнопок используется компонент `Button` из библиотеки `Shadcn`
    
    - Компонент `Button` оборачивается в компонент `Link` из библиотеки `next/link` для переадресации пользователя
    
    - Стилизация компонента `Button` и добавление иконки из `lucide-react`

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -2,6 +2,9 @@
 
 import { UserButton } from '@clerk/nextjs';
 import { usePathname } from 'next/navigation';
+import { Button } from './ui/button';
+import Link from 'next/link';
+import { LogOut } from 'lucide-react';
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
@@ -10,6 +13,20 @@ const NavbarRoutes = () => {
 
   return (
     <div className='ml-auto flex items-center gap-x-2'>
+      {isTeacherPage || isCoursePage ? (
+        <Link href={'/'}>
+          <Button size={'sm'} variant={'ghost'}>
+            <LogOut className='mr-2 h-4 w-4' />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href={'/teacher/courses'}>
+          <Button size={'sm'} variant={'ghost'}>
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
       <UserButton afterSignOutUrl='/' />
     </div>
   );
