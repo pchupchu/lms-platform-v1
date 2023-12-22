e70d507f9c96bdd26a60d299d5e7f89bedaaaaf6
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-22 13:34:02 +0300

    Создание переменных в `NavbarRoutes`
    - Создание переменной `pathname` для определения текущего адреса пользователя с помощью хука `usePathname` из библиотеки `next/navigation`
    
    - Создание переменной `isTeacherPage` для того, чтобы определить, связан ли текущий адрес с `TeacherMode`. Данная переменная создается с помощью метода `startsWith`
    
    - Создание переменной `isCoursePage`, чтобы определить, связан ли текущий адрес с режимом просмотра курса. Данная переменная создается с помощью метода `includes`

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import { UserButton } from '@clerk/nextjs';
+import { usePathname } from 'next/navigation';
 
 const NavbarRoutes = () => {
+  const pathname = usePathname();
+  const isTeacherPage = pathname.startsWith('/teacher');
+  const isCoursePage = pathname.includes('/chapter');
+
   return (
     <div className='ml-auto flex items-center gap-x-2'>
       <UserButton afterSignOutUrl='/' />
