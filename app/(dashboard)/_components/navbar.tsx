e204a9b68af942355fb24709e4c933ce59a729fd
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-23 15:07:42 +0300

    Исправлена высота Navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import MobileSidebar from './mobile-sidebar';
 
 const Navbar = () => {
   return (
-    <div className='flex items-center border-b bg-white p-4 shadow-sm'>
+    <div className='flex h-full items-center border-b bg-white p-4 shadow-sm'>
       <MobileSidebar />
       <NavbarRoutes />
     </div>
