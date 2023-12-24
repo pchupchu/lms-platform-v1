09298b55f9389d8aa2c230c3bf987af5d5f29d5b
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-24 14:24:24 +0300

    Добавление свойств и функции-обработчика для тега `<form>`
    - Добавление свойств `required` и  `minLenght` для `инпутов`
    
    - Добавление свойства `onSubmit` для того, чтобы вызвать функцию-обработчик, которая срабатывает только при наступлении события `submit`, т.е. отправки формы
    Также эту функцию-обработчик можно вынести в отдельную функцию и присвоить ее значение свойству `onSubmit`
    
    - При срабатывании функции-обработчика события `submit` окно браузера перезагружается, т.к. это его действие по умолчанию. Чтобы предотвратить данное действие, нужно явно вызвать `preventDefault`

--- app/(sandbox)/(routes)/form-without-react-hook-form/page.tsx
+++ app/(sandbox)/(routes)/form-without-react-hook-form/page.tsx
@@ -1,24 +1,31 @@
 'use client';
 
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 const FormWithoutReactHookFormPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className='flex flex-col gap-y-2'>
+    <form onSubmit={onSubmit} className='flex flex-col gap-y-2'>
       <input
         type='email'
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        required
         placeholder='Email'
         className='rounded px-4 py-2'
       />
       <input
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        required
+        minLength={6}
         type='password'
         placeholder='Password'
         className='rounded px-4 py-2'
@@ -26,6 +33,8 @@ const FormWithoutReactHookFormPage = () => {
       <input
         value={confirmPassword}
         onChange={(e) => setConfirmPassword(e.target.value)}
+        required
+        minLength={6}
         type='password'
         placeholder='Confirm password'
         className='rounded px-4 py-2'
