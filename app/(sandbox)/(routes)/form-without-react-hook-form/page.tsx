c81f40fa151a209be3dfc67744cb59cd50b11154
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-24 12:57:19 +0300

    Верстка формы в компоненте `FormWithoutReactHookFormPage`

--- app/(sandbox)/(routes)/form-without-react-hook-form/page.tsx
+++ app/(sandbox)/(routes)/form-without-react-hook-form/page.tsx
@@ -1,5 +1,24 @@
 const FormWithoutReactHookFormPage = () => {
-  return <div>This is a form without ReactHookForm</div>;
+  return (
+    <form className='flex flex-col gap-y-2'>
+      <input type='email' placeholder='Email' className='rounded px-4 py-2' />
+      <input
+        type='password'
+        placeholder='Password'
+        className='rounded px-4 py-2'
+      />
+      <input
+        type='password'
+        placeholder='Confirm password'
+        className='rounded px-4 py-2'
+      />
+      <button
+        type='submit'
+        className='rounded bg-green-500 py-2 text-lg font-medium text-white disabled:bg-gray-500'>
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default FormWithoutReactHookFormPage;
