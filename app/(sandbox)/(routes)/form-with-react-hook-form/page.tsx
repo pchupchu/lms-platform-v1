650543b2c8a2a959edd4e1e1b695f14448372f57
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-25 21:39:45 +0300

    Отображение массива ошибок для каждого поля формы
    - Отменена валидация формы для отображения нативных ошибок браузера
    
    - Добавлен паттерн для валидации `email` и кастомное сообщение
    
    - Отображение ошибок полей ввода формы, которые можно получить из хука `useForm`.  Рендер массива ошибок происходит при условии, что внутри него есть свойство с названием поля формы с помощью оператора `&&`. Если ошибка есть, то отображается тег с текстом сообщения ошибки

--- app/(sandbox)/(routes)/form-with-react-hook-form/page.tsx
+++ app/(sandbox)/(routes)/form-with-react-hook-form/page.tsx
@@ -7,7 +7,7 @@ const FormWithReactHookFormPage = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting, isValid },
+    formState: { errors, isSubmitting },
     reset,
     getValues,
   } = useForm();
@@ -15,7 +15,7 @@ const FormWithReactHookFormPage = () => {
   const onSubmit = async (data: FieldValues) => {
     const result = await new Promise((resolve, reject) =>
       setTimeout(() => {
-        resolve('Успешный результат');
+        resolve('Success');
       }, 2000),
     );
     console.log(result);
@@ -23,15 +23,25 @@ const FormWithReactHookFormPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-y-2'>
+    <form
+      noValidate
+      onSubmit={handleSubmit(onSubmit)}
+      className='flex flex-col gap-y-2'>
       <input
         {...register('email', {
           required: 'Email is required',
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: 'Incorrect email',
+          },
         })}
         type='email'
         placeholder='Email'
         className='rounded px-4 py-2'
       />
+      {errors.email && (
+        <p className='text-red-500'>{`${errors.email.message}`}</p>
+      )}
       <input
         {...register('password', {
           required: 'Password is required',
@@ -44,6 +54,9 @@ const FormWithReactHookFormPage = () => {
         placeholder='Password'
         className='rounded px-4 py-2'
       />
+      {errors.password && (
+        <p className='text-red-500'>{`${errors.password.message}`}</p>
+      )}
       <input
         {...register('confirmPassword', {
           required: 'Confirm password is required',
@@ -52,8 +65,11 @@ const FormWithReactHookFormPage = () => {
         placeholder='Confirm password'
         className='rounded px-4 py-2'
       />
+      {errors.confirmPassword && (
+        <p className='text-red-500'>{`${errors.confirmPassword.message}`}</p>
+      )}
       <button
-        disabled={isSubmitting || !isValid}
+        disabled={isSubmitting}
         type='submit'
         className='rounded bg-green-500 py-2 text-lg font-medium text-white disabled:bg-gray-500'>
         Submit
