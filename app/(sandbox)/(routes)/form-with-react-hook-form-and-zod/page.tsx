e699f3da132a514ce167e698e536d0307e9d51d0
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-26 12:09:25 +0300

    Проверка паролей на соответствие
    - Добавляется функция `.refine()` для проверки совпадения паролей в полях `password` и `confirmPassword`
    
    - Добавляется сообщение об ошибке и указывается путь, куда будет выводиться ошибка для несовпадающих паролей

--- app/(sandbox)/(routes)/form-with-react-hook-form-and-zod/page.tsx
+++ app/(sandbox)/(routes)/form-with-react-hook-form-and-zod/page.tsx
@@ -4,11 +4,16 @@ import { FormEvent, useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 import { z } from 'zod';
 
-const signUpSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
-  confirmPassword: z.string(),
-});
+const signUpSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  });
 
 const FormWithReactHookFormPageAndZod = () => {
   const {
