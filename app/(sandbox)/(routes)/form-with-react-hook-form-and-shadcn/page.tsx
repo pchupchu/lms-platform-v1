67a92b5adda471f36278288039542927496dc014
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-27 13:56:00 +0300

    Отображение компонента `Form`
    - Установка компонентов `Button` и `Input`  из библиотеки `shadcn`
    
    - Импортированы и добавлены в разметку компоненты `Button`, `Input`, `Form`, `FormControl`, `FormField`, `FormItem`, `FormLabel` и `FormMessage` из модуля `@/components/ui`
    
    - Значение переменной `form` передается с помощью спред-оператора в компонент `Form`
    
    - Создается нативный  тег `form` , которому добавляется проп `onSubmit` и проп `className` для стилизация полей формы
    
    - Метод `handleSubmit` выполняет валидацию данных формы и, в случае отсутствия ошибок, вызывает функцию, переданную ей в качестве параметра
    
    - Каждое поле формы оборачивается компонентом `FormFiefd`, в который передаются следующие обязательные пропы `control` для интеграции данного поля с библиотекой `react-hook-form`,  `name` для регистрации данного поля и `render` для отображения компонента `FormItem`. Проп `name` вводится с помощью автокомплита, т.к. поля формы типизированы с помощью `FormSchemaType`
    - `FormItem` оборачивает следующие компоненты:  `FormControl`, `FormLabel` и `FormMessage`
    
    - `FormControl` оборачивает поле ввода формы
    
    - Значение `field` передается с помощью спред-оператора в компонент `Input`

--- app/(sandbox)/(routes)/form-with-react-hook-form-and-shadcn/page.tsx
+++ app/(sandbox)/(routes)/form-with-react-hook-form-and-shadcn/page.tsx
@@ -1,5 +1,15 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
@@ -31,7 +41,27 @@ const FormWithReactHookFormAndShadcn = () => {
     console.log(result);
   };
 
-  return <div>This is a new form</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder='email' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default FormWithReactHookFormAndShadcn;
