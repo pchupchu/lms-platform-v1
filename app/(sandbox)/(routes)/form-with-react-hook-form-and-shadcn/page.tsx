2d9b012d5ff405c0b2588d13be2f1eece66d780a
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-27 12:02:16 +0300

    Создание схемы с помощью `zod`
    - Импорт `z` из библиотеки `zod`
    
    -  Создается схема с использованием `zod` для валидации данных формы
    
    - Все свойства объекта `z.object` по умолчанию обязательны для заполнения
    
    - Все свойства подвергаются валидации как строки
    
    - Добавляется проверка на соответствие формату email для свойства `email`
    
    - Устанавливается минимальное значение для поля пароля

--- app/(sandbox)/(routes)/form-with-react-hook-form-and-shadcn/page.tsx
+++ app/(sandbox)/(routes)/form-with-react-hook-form-and-shadcn/page.tsx
@@ -1,5 +1,13 @@
 'use client';
 
+import * as z from 'zod';
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+  confirmPassword: z.string(),
+});
+
 const FormWithReactHookFormAndShadcn = () => {
   return <div>This is a new form</div>;
 };
