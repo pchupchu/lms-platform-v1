d9eefa22d2db2c8c02d0373a109a3f61d6f434de
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-28 20:23:06 +0300

    Создание обертки и заголовка компонента `CreatePage`
    - Добавление тега-обертки и его стилизация для выравнивания содержимого по центру страницы на расширении `768px` и больше
    
    - Добавление заголовка и описания компонента `CreatePage`

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -25,7 +25,17 @@ const CreatePage = () => {
     console.log(values);
   };
 
-  return <div>This is a Create page</div>;
+  return (
+    <div className='mx-auto flex h-full max-w-5xl p-6 md:items-center md:justify-center'>
+      <div>
+        <h1 className='text-2xl'>Name your course</h1>
+        <p className='text-sm text-slate-600'>
+          How would you like to name your course? Don&apos;t worry, you can
+          change this later
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePage;
