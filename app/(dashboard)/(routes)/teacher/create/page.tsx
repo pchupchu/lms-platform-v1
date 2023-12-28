73bc723055a84e72d9b077a64c1d1f8fca05d6c2
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-28 19:29:18 +0300

    Добавление метода `trim`
    Убран лишний экспорт из компонента `CreatePage`

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { TypeOf, z } from 'zod';
+import { z } from 'zod';
 
 const formSchema = z.object({
-  title: z.string().min(1, 'Title is required'),
+  title: z.string().trim().min(1, 'Title is required'),
 });
 
 type FormSchemaType = z.infer<typeof formSchema>;
