51c31821ec335b3c70964e2eb70139f838e601bd
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-28 19:14:36 +0300

    Создание схемы для валидации данных формы
    - Добавление маркировки `use client` компоненту `CreatePage`, чтобы использовать хук  `useForm`
    
    - Импортирование библиотеки `zod`
    
    - Создание переменной `formSchema` для валидации поля `title`. Данное поле является по умолчанию обязательным, оно должно содержать не менее одного символа и быть строкой. При ошибке возникнет кастомное сообщение `Title is required`
    
    Данная схема располагается вне компонента  `CreatePage`

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import { z } from 'zod';
+
+const formSchema = z.object({
+  title: z.string().min(1, 'Title is required'),
+});
+
 const CreatePage = () => {
   return <div>This is a Create page</div>;
 };
