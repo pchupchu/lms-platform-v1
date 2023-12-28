517fb9ade8a22649c557a8cb275aafd2c500b2da
Marina Malyarchuk <pchupchu@yandex.ru>
2023-12-28 19:24:55 +0300

    Определение типа FormSchemaType`
    Определение типа и синтаксиса `FormSchemaType` с помощью метода-утилиты `infer` и синтаксиса `<typeof>`

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { z } from 'zod';
+import { TypeOf, z } from 'zod';
 
 const formSchema = z.object({
   title: z.string().min(1, 'Title is required'),
 });
+
+type FormSchemaType = z.infer<typeof formSchema>;
 
 const CreatePage = () => {
   return <div>This is a Create page</div>;
