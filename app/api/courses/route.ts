3d929c9c95cd35b651f80da1a0a0490ea68d0d2c
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-06 15:04:10 +0300

    Добавление проверки на наличие `userId`
    Если такого `userId` не существует, то возвращается ошибка с кодом `401` и текстом `Unauthorized`

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -5,6 +5,13 @@ export async function POST(request: NextRequest) {
   try {
     const { userId } = auth();
     const { title } = await request.json();
+
+    /*
+    WARN: зачем эта проверка? 
+    */
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
   } catch (error) {
     console.log('[COURSES]', error);
     return new NextResponse('Internal error', { status: 500 });
