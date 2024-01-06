2f95222fb2a9e5553c2979fdc6d83472ffa0efc3
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-06 14:49:10 +0300

    Получение значений `userId` и `title`
    Импорт `auth` из библиотеки  `@clerk/nextjs`
    
    - Получение значение `userId` путем деструктуризации значения функции `auth`
    
    - Метод `JSON` парсит тело HTTP-запроса (request) в объект `JavaScript`, после чего получаем значение `title` путем деструктуризации

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
 
 export async function POST(request: NextRequest) {
   try {
+    const { userId } = auth();
+    const { title } = await request.json();
   } catch (error) {
     console.log('[COURSES]', error);
     return new NextResponse('Internal error', { status: 500 });
