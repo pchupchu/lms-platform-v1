6be27327fe6e7c5ec8b76ca7f064f22dfedb745b
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-14 13:59:58 +0300

    Добавлен комментарий к файлу db.ts, поясняющий исправление бага Prisma, который возникает при hot-reload

--- lib/db.ts
+++ lib/db.ts
@@ -5,5 +5,7 @@ declare global {
 }
 
 export const db = globalThis.prisma ?? new PrismaClient();
+// Оператор ?? означает что функция new PrismaClient() выполняется по дефолту в случае если globalThis.prisma будет null или Undefined. This allows hot reloading in development to reuse the existing client instead of creating a new one each time.
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
+// Если мы находимся в режиме разработки, то мы записываем объект prisma в глобальный объект globalThis.prisma
