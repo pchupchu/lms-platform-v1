a1f9494d89a10f32a0b88f86028dfbbd16d5624b
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-14 14:26:42 +0300

    Добавление значений формы в базу данных
    - Создание переменной `course`, которой присваивается асинхронный вызов инстанса `Prisma client`
    
    - Используя метод `create`, который вызывается у модели `course`, создается новая запись в базе данных. Для этого в параметр функции передается объект со свойством `data`, которому присваиваются обязательные значения данного курса

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { auth } from '@clerk/nextjs';
+import { db } from '@/lib/db';
 
 export async function POST(request: NextRequest) {
   try {
@@ -12,6 +13,8 @@ export async function POST(request: NextRequest) {
     if (!userId) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
+
+    const course = await db.course.create({ data: { userId, title } });
   } catch (error) {
     console.log('[COURSES]', error);
     return new NextResponse('Internal error', { status: 500 });
