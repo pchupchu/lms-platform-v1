3d88b983cda58121db888033fb11e9d1b6315f9e
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-15 14:05:22 +0300

    Создание запроса к базе данных, используя инстанс `Prisma client`
    - Реализация синтаксиса `async/await` в компоненте `CourseIdPage`
    
    - Выполнение запроса на получение курса из базы данных с использованием метода `findUnique`, чтобы получить уникальный курс, в котором `id` курса соответствует параметру `courseId`, полученному из `URL`
    
    - Получение значения `userId`, используя функцию `auth` из библиотеки `Clerk`
    
    - Добавление условия для проверки наличия курса и подтверждения, что текущий пользователь является создателем данного курса
    
    - Добавление переадресации на главную страницу в случае, если курс не найден или пользователь не является создателем данного курса

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,11 +1,22 @@
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
 interface CourseIdProps {
   params: {
     courseId: string;
   };
 }
 
-const CourseIdPage = ({ params }: CourseIdProps) => {
+const CourseIdPage = async ({ params }: CourseIdProps) => {
   const { courseId } = params;
+  const { userId } = auth();
+
+  const course = await db.course.findUnique({ where: { id: courseId } });
+
+  if (!course || course.userId !== userId) {
+    return redirect('/');
+  }
 
   return <div>{courseId}</div>;
 };
