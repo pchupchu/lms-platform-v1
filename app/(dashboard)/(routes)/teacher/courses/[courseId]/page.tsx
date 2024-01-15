5d33326e235f04d22c4d5304d1e403221ad3bc8f
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-15 15:19:08 +0300

    Добавление разметки для отображения заголовка и `Requred Fields` внутри компонента `CourseIdPage`

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -32,7 +32,18 @@ const CourseIdPage = async ({ params }: CourseIdProps) => {
   ).length;
   const completionText = `(${completedFields}/${totalFields})`;
 
-  return <div>{completionText}</div>;
+  return (
+    <div className='p-6'>
+      <div className='flex items-center justify-between'>
+        <div className='flex flex-col gap-y-2'>
+          <h1 className='text-2xl font-medium'>Course Setup</h1>
+          <span className='text-sm text-slate-700'>
+            Comlete all fields {completionText}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseIdPage;
