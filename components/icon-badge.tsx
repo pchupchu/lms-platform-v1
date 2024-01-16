eaf68a2a848f0634987eb9bd6f8c1ab8562cfce0
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-16 13:31:01 +0300

    Использование `cva` функции-утилиты для добавления кастомной стилизации
    - Создание переменной  `backgroundVariants`, которой присваивается значение вызова функции `cva`, импортированной из библиотеки `class-variance-authority`
    
    - Функция `cva` принимает общие дефолтные значения стилей в качестве первого параметра и объект с настройками, который позволяет определять кастомные стили и их дефолтные значения, в качестве второго
    
    - В этом случае `backgroundVariants` возвращает общие дефолтные значения стилей функции `cva` и дополнительно настраивается с параметрами `variant` и `size`, которые имеют соответствующие значения по умолчанию и кастомные параметры стилей

--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -1,5 +1,25 @@
+import { VariantProps, cva } from 'class-variance-authority';
+import { LucideIcon } from 'lucide-react';
+
+const backgroundVariants = cva(
+  'flex items-center justify-center rounded-full border-black',
+  {
+    variants: {
+      variant: {
+        default: 'bg-sky-100',
+        success: 'bg-emerald-100',
+      },
+      size: { default: 'p-2', sm: 'p-1' },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+);
+
 const IconBadge = () => {
-  return <div>This is an Icon Badge</div>;
+  return <div>XX</div>;
 };
 
 export default IconBadge;
