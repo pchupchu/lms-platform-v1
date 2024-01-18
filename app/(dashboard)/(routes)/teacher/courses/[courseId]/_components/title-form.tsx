e399fe3a382277ff57bf95a99c85029a7dee573b
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-18 12:44:48 +0300

    Создание интерфейса `TitleFormProps` и присваивание его объекту пропс компонента `TitleForm`

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-const TitleForm = () => {
+interface TitleFormProps {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+}
+
+const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
   return <div>This is a title form</div>;
 };
 
