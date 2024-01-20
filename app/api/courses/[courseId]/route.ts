ec19bfa850a2f21717d5c771906d2edc0e5cde52
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-20 13:30:34 +0300

    Обработка ошибки на сервере с помощью `NextResponse`
    В случае ошибки в блоке `try/catch` возвращаю `NextResponse` с текстом `Internal Error` и статусом `500`

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,8 +1,9 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function PATCH(request: NextRequest) {
   try {
   } catch (error) {
     console.log('[COURSE_ID]', error);
+    return new NextResponse('Internal Error', { status: 500 });
   }
 }
