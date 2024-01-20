9d2a02a3910dc8f580891fd6295d298f64c8ccd7
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-20 14:34:39 +0300

    Получение значений `userId` и преобразованного в объект тела `HTTP-запроса`
    - Вызываю функцию `auth` из библиотеки  `clerk`, чтобы получить значение `userId` с помощью деструктуризации
    
    - Создаю переменную `values` и присваиваю ей значение асинхронного вызова метода `json`, который обрабатывает входящий `HTTP-запрос`

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,7 +1,10 @@
+import { auth } from '@clerk/nextjs';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function PATCH(request: NextRequest) {
   try {
+    const { userId } = auth();
+    const values = await request.json();
   } catch (error) {
     console.log('[COURSE_ID]', error);
     return new NextResponse('Internal Error', { status: 500 });
