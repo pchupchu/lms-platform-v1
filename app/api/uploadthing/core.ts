c53d28a7b9b2235413b657a9ee18caa633b93bd6
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-30 13:26:44 +0300

    Создала функцию `handleAuth` для проверки аутентификации пользователя
    - Создала функцию `handleAuth` с помощью сниппета
    
    - Импортировала и вызвала  функцию `auth` из `@clerk/nextjs`, она возвращает объект с информацией об аутентификации пользователя. С помощью деструктуризации получила значение свойства `userId`
    
    - Добавила условие с помощью оператора `if`, которое выполняется в случае отсутствия `userId`
    
    - Если `userId` отсутствует, то выбрасываю ошибку с текстом `Unauthorised`. Это приведет к тому, что пользователь не сможет загрузить файл, т.к. не прошел аутентификацию
    
    - Если `userId` присутствует, то возвращаю объект с информацией о пользователе, в данном случае его `userId`. Эта информации будет доступна в свойстве `metadata` объекта, который передается в качестве аргумента колбэку, вызываемому после успешной загрузки файла на сервер `.onUploadComplete(({metadata}) => {})`

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,6 +1,19 @@
+import { auth } from '@clerk/nextjs';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 
 const f = createUploadthing();
+
+const handleAuth = () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    // If you throw, the user will not be able to upload
+    throw new Error('Unauthorised');
+  }
+
+  // Whatever is returned here is accessible in onUploadComplete as metadata
+  return { userId };
+};
 
 // FileRouter for your app, can contain multiple FileRoutes. Define as many FileRoutes as you like, each with a unique routeSlug.
 export const ourFileRouter = {} satisfies FileRouter;
