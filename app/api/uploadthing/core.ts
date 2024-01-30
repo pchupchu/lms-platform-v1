0c54753d355adbcb1db15e6c71c8cafd5989215c
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-30 13:54:08 +0300

    Создание маршрута `courseImage` для обработки изображений на сервере
    - Создаю маршрут с именем `courseImage`, который использует функцию `f`, созданную с использованием `createUploadthing`, чтобы настроить параметры обработки изображений. В данном случае устанавливаю максимальный размер `maxFileSize` и максимальное количество файлов `maxFileCount`, которые можно загрузить согласно примеру из документации
    
    - К результату предыдущего вызова применяется метод `middleware` для выполнения дополнительной логики перед загрузкой файла. В данном случае используется функция `handleAuth`, которая проверяет аутентификацию пользователя прежде, чем авторизовать его на загрузку файла
    
    - Последним в данной последовательности выполняется метод `onUploadComplete`. Этот метод предоставляет колбэк функцию, которая будет вызвана после успешной загрузки файла. В нашем случае колбэк пустой, но обычно здесь размещаются дополнительные действия, которые должны выполняться после завершения загрузки файла на сервер

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -16,6 +16,13 @@ const handleAuth = () => {
 };
 
 // FileRouter for your app, can contain multiple FileRoutes. Define as many FileRoutes as you like, each with a unique routeSlug.
-export const ourFileRouter = {} satisfies FileRouter;
+export const ourFileRouter = {
+  // Set permissions and file types for this FileRoute
+  courseImage: f({ image: { maxFileSize: '16MB', maxFileCount: 1 } })
+    // This code runs on your server before upload
+    .middleware(() => handleAuth())
+    // Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
