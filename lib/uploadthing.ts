b5167523a466f365162b7d534eb94584f92036a7
Marina Malyarchuk <pchupchu@yandex.ru>
2024-01-31 13:26:11 +0300

    Создание `UploadThing` компонентов согласно документации
    - Создаю файл `uplodthing.ts` в папке `lib`
    
    - Импортирую тип `OurFileRouter` из файла `core.ts`
    
    - Импортирую функцию `generateUploadDropzone` из библиотеки `@uploadthing/react`
    
    - Создаю и экспортирую компонент `UploadDropzone`, используя вызов функции `generateUploadDropzone`
    - В качестве дженерика передаю тип `OurFileRouter`, что позволит типизировать передаваемые в данный компонент значения

---
+++ lib/uploadthing.ts
@@ -0,0 +1,4 @@
+import type { OurFileRouter } from '@/app/api/uploadthing/core';
+import { generateUploadDropzone } from '@uploadthing/react';
+
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
