95aad47f4585a6ee5761f364b3566a9b4eac96cc
Marina Malyarchuk <pchupchu@yandex.ru>
2024-02-02 13:53:48 +0300

    Добавила компонент `UploadDropzone` в jsx-разметку `FileUpload` и добавила ему следующие свойства согласно примера из документации
    	- `endpoint` нужен для определения маршрута загрузки и способов обработки файлов на сервере. Его значение мы получаем из объекта пропс
    
    	- `onClientUploadComplete` - передаю в него анонимную функцию, которая будет вызвана в случае успешной загрузки файла. В качестве параметра она принимает массив с информацией о загруженных файлах, из которого необходимо получить значение свойства `url` у первого элемента. Данное значение мы передаем при вызове функции `onChange`, которую получаю из объекта пропс. В случае успешной загрузки картинки на сервер будет появляться компонент `toast` с текстом `Upload Completed`
    
      - `onUploadError` - передаю в него анонимную функцию, которая будет вызвана в случае ошибки загрузки файла на сервер. Если ошибка произойдет, то вывожу ее в консоль и отображаю компонент `toast` с текстом `Something went wrong`

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,6 +2,7 @@
 
 import { UploadDropzone } from '@/lib/uploadthing';
 import type { OurFileRouter } from '@/app/api/uploadthing/core';
+import toast from 'react-hot-toast';
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
@@ -9,7 +10,22 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
-  return <div></div>;
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        // Do something with the response
+        console.log('[RESPONSE_FROM_UPLOADTHING]', res);
+        onChange(res[0].url);
+        toast.success('Upload Completed');
+      }}
+      onUploadError={(error: Error) => {
+        // Do something with the error.
+        toast.error('Something went wrong');
+        console.log('[UPLOADTHING]', error.message);
+      }}
+    />
+  );
 };
 
 export default FileUpload;
