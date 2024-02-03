778d762fd1f3082ff94437dd5c19bce3db50823d
Marina Malyarchuk <pchupchu@yandex.ru>
2024-02-03 13:49:23 +0300

    Отображение дефолтной картинки, пока картинка еще не загружена на сервер
    Если мы находимся НЕ в режиме редактирования и мы НЕ ПОЛУЧИЛИ `url` картинки в объекте `initialData`, то отображается `div-wrapper` с иконкой `ImageIcon` по центру и соответствующей стилизацией

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { PencilIcon, PlusCircle } from 'lucide-react';
+import { ImageIcon, PencilIcon, PlusCircle } from 'lucide-react';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
@@ -56,7 +56,11 @@ const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
           )}
         </Button>
       </div>
-      {!isEditing && <></>}
+      {!isEditing && !initialData.imageUrl && (
+        <div className='flex h-60 items-center justify-center rounded-md bg-slate-200'>
+          <ImageIcon className='h-10 w-10 text-slate-500' />
+        </div>
+      )}
       {isEditing && <></>}
     </div>
   );
