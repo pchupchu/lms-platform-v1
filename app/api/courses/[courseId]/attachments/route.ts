ca27513a42c14daed2c754b3d5717fa02b8f8f2b
Marina Malyarchuk <pchupchu@yandex.ru>
2024-02-08 19:34:29 +0300

    Обработка ошибки в блоке `catch` с помощью `NextResponse`
    - Импорт `NextResponse` из библиотеки `next/server`.
    
    - В случае ошибки в блоке `try` возвращается `new NextResponse` с текстом `Internal Error` и статусом `500`.

--- app/api/courses/[courseId]/attachments/route.ts
+++ app/api/courses/[courseId]/attachments/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 interface ContextProps {
   params: {
@@ -10,5 +10,6 @@ export async function POST(request: NextRequest, { params }: ContextProps) {
   try {
   } catch (error) {
     console.log('[ATTACHMENTS]', error);
+    return new NextResponse('Internal Error', { status: 500 });
   }
 }
