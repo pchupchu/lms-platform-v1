b8c10168c87ffc82ad2a778eb0ee541fa5121cc3
Marina Malyarchuk <pchupchu@yandex.ru>
2024-03-26 12:49:24 +0300

    Проверка того, кто создает новую главу курса, чтобы убедиться, что он является создателем курса
    - Создана переменная `courseOwner`, которая будет содержать результат запроса к базе данных.
    
    - Создан асинхронный запрос к базе данных, используя:
      - Синтаксис `async/await`, позволяющий совершать асинхронные запросы.
      - Объект `db` - экземпляр класса `PrismaClient`, представляющий собой объект со свойствами и методами, необходимыми для доступа к базе данных.
      - Модель `course` - таблица в базе данных.
      - Метод `findUnique`, который ищет уникальную запись в модели `course`, согласно заданному условию.
      - Свойство `where` - это опция, которая определяет условие, по которому нужно найти запись в модели `course`. В данном случае осуществляется поиск уникальной записи `сourse`, где `id` совпадает с `params.courseId`, а `userId` соответствует значению `userId`, предоставленному библиотекой `clerk`.
    
    - Если `courseOwner` не найден, то возвращается `new NextResponse` со статусом`401`  и сообщением `Unauthorized`.

--- app/api/courses/[courseId]/chapters/route.ts
+++ app/api/courses/[courseId]/chapters/route.ts
@@ -1,3 +1,4 @@
+import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -13,6 +14,14 @@ export async function POST(request: NextRequest, { params }: ContextProps) {
     const { chaptersTitle } = await request.json();
 
     if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: { id: params.courseId, userId },
+    });
+
+    if (!courseOwner) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
   } catch (error) {
