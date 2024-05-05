8d65aa6ebcf5904ec94547a18c9cfd6767c63b70
Marina Malyarchuk <pchupchu@yandex.ru>
2024-05-05 18:22:40 +0300

    Создание базовой структуры компонента `ChapterIdPage` для отображения на динамическом роуте
     - Используя динамический сегмент, создана папка `[chapterId]` для того, чтобы получить идентификатор главы из объекта `params` и найти её в базе данных.
    
    - Используя сниппет, cоздан файл `page.tsx` на роуте `teacher/courses/[courseId]/chapters/[chapterId]`
    
    - Добавлен интерфейс `ChapterIdPageProps`, который в свойстве `params` принимает объект со свойствами `courseId`  и `chapterId` и значениями типа  `string`.
    
    - Из объекта `props` с помощью деструктуризации получено значение свойства `params` и присвоено значение интерфейса.
    
    - Из объекта `params` с помощью деструктуризации получены значения свойств `courseId` и `chapterId`.
    
    - Из вызова функции `auth()`, импортированной из модуля `@clerk/nextjs`,  получено значение свойства `userId` .

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,12 +1,18 @@
-interface ChapterIdProps {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChapterIdPageProps {
    params: {
     courseId: string;
     chapterId: string;
   };
 }
 
-const ChapterIdPage =  ({ params }: ChapterIdProps) => {
+const ChapterIdPage = async ({ params }: ChapterIdPageProps) => {
   const { courseId, chapterId } = params;
+
+  const { userId } = auth();
+
    return <div>{chapterId}</div>;
   };
   
