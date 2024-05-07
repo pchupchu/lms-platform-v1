c1f51954e79804cfd48cbac92d86073c39c2ebdc
Marina Malyarchuk <pchupchu@yandex.ru>
2024-05-07 13:58:08 +0300

    Получение значений `userId` и входящего `HTTP-запроса`
    -  Деструктурировано значение `userId` из вызова функции `auth()`, полученной из модуля `@clerk/nextjs`.
    
    - Создана переменная `values` и ей присвоено значение асинхронного вызова метода `json`, который обрабатывает входящий `HTTP-запрос`.
    
    - Если `userId` не существует, то возвращается `new NextResponse` со  статусом  `401` и сообщением `Unauthorized`. Это необходимо для того, чтобы избежать ошибки тайпскрипта во время запроса к базе данных, т.к. `userId` может быть `null`.

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,3 +1,4 @@
+import { auth } from '@clerk/nextjs';
 import { NextRequest, NextResponse } from 'next/server';
 
 interface ContextProps {
@@ -9,6 +10,12 @@ interface ContextProps {
 
 export async function PATCH(request: NextRequest, { params }: ContextProps) {
   try {
+    const { userId } = auth();
+    const values = await request.json();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
   } catch (error) {
     console.log('[CHAPTER_ID]', error);
     return new NextResponse('Internal Error', { status: 500 });
