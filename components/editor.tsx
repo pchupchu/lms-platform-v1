e5be4c723612b1804839f461619b3f2997682bd0
Marina <pchupchu@yandex.ru>
2024-06-06 19:55:06 +0300

    Создание компонента `Editor`
    - В папке `components`, которая предназначена для использования компонентов во всем проекте, создан компонент `Editor` с помощью сниппета.
    
    - Добавлена маркировка `use client` для использования хука `useEditor`.
    
    - Добавлен интерфейс компонента, который включает в себя следующие свойства:
    	- `value` - значение с типом `string`;
    	- `onChange` - это функция, которая в качестве аргумента принимает значение с типом `string` и ничего не возвращает.
    
    -  Из объекта `props` деструктурированы необходимые свойства и присвоено значение интерфейса `EditorProps`.

---
+++ components/editor.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+interface EditorProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const Editor = ({ value, onChange }: EditorProps) => {
+  return <></>;
+};
+
+export default Editor;
