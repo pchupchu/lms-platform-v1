f36eec3a08b6f045fe2f55e518a71096f2f474d5
Marina <pchupchu@yandex.ru>
2024-06-06 20:35:39 +0300

    Создание переменной `editor`
    - Создана переменная `editor`, которой присвоено значение вызова хука `useEditor`.
    
    - В качестве аргумента хук принимает объект со свойствами:
    	- `extensions` - массив c  расширениями, которые используются в редакторе. В качестве базового расширения, которое содержит в себе основной функционал, используется `StarterKit` из библиотеки `@tiptap/starter-kit`.
    	- `content` - информация, которая отображается в редакторе, она хранится в локальном стейте со значением `value`, полученным из `React Hook Form`.
    	- `editorProps` - с помощью свойств `attributes` и `class` можно добавить стилизацию компонента `EditorContent`.
    	- `onUpdate` - функция, которая срабатывает при каждом изменении значения компонента `EditorContent`. С помощью деструктуризации параметра данной функции получено значение `editor`. Для того, чтобы получить текущую разметку компонента у данного значения вызывается метод `getHTML()` и присваивается переменной `data`. Если редактор не содержит контента, то  функция `onChange` вызывается с пустой строкой, чтобы отобразить ошибку `React Hook Form`. Иначе  полученная информация сохранится в локальный стейт.
    
    - Если компонент загружается на сервере, то возвращается `null`.
    
    - `EditorContent` импортирован из библиотеки `@tiptap/react` и добавлен в  jsx-разметку. В качестве пропса передано значение переменной `editor`.

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,12 +1,40 @@
 'use client';
 
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+
 interface EditorProps {
   value: string;
   onChange: (value: string) => void;
 }
 
 const Editor = ({ value, onChange }: EditorProps) => {
-  return <></>;
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: value,
+    editorProps: {
+      attributes: {
+        class:
+          'flex flex-col min-h-[180px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+      },
+    },
+    onUpdate: ({ editor }) => {
+      const data = editor.getHTML();
+      if (editor.isEmpty) {
+        onChange('');
+      } else {
+        onChange(data);
+      }
+    },
+  });
+
+  if (!editor) return null;
+
+  return (
+    <>
+      <EditorContent editor={editor} />
+    </>
+  );
 };
 
 export default Editor;
