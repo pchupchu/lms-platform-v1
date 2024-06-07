98daa7e88aa0833d169eaced887608d361ba0aa8
Marina <pchupchu@yandex.ru>
2024-06-07 19:27:29 +0300

    Добавление кнопок для редактирования текста
    - С помощью команды в терминале добавлен компонент `Toggle` из библиотеки `shadcn`.
    
    - Добавлен компонент `Toggle` из библиотеки `shadcn`.
    
    - Данный компонент в качестве пропсов принимает следующие свойства:
    	- `size` - размер кнопки;
    	- `variant` - стиль кнопки;
    	- `pressed` - условие, при котором данная кнопка считается активной;
    	- `onPressedChange` - функция, которая вызывается при клике на данную кнопку.
    
    - В зависимости от кнопки у объекта `editor` вызывается цепочка методов с соответствующим функционалом [согласно документации](https://tiptap.dev/docs/editor/extensions).
    
    - Компонент `Toggle` в качестве `children` принимает иконку из библиотеки `lucide-react`.

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,4 +1,18 @@
 import { Editor } from '@tiptap/react';
+import { Toggle } from './ui/toggle';
+import {
+  Bold,
+  Code,
+  Heading,
+  Italic,
+  List,
+  ListOrdered,
+  Quote,
+  Redo,
+  Strikethrough,
+  Underline,
+  Undo,
+} from 'lucide-react';
 
 interface ToolbarProps {
   editor: Editor;
@@ -7,7 +21,97 @@ interface ToolbarProps {
 const Toolbar = ({ editor }: ToolbarProps) => {
   return (
     <div className='flex items-start'>
-      <div className='flex w-full flex-wrap items-center justify-start gap-1 rounded-md border border-input bg-white p-1'></div>
+      <div className='flex w-full flex-wrap items-center justify-start gap-1 rounded-md border border-input bg-white p-1'>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('heading', { level: 2 })}
+          onPressedChange={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }>
+          <Heading className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('bold')}
+          onPressedChange={() => editor.chain().focus().toggleBold().run()}>
+          <Bold className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('italic')}
+          onPressedChange={() => editor.chain().focus().toggleItalic().run()}>
+          <Italic className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('underline')}
+          onPressedChange={() =>
+            editor.chain().focus().toggleUnderline().run()
+          }>
+          <Underline className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('strike')}
+          onPressedChange={() => editor.chain().focus().toggleStrike().run()}>
+          <Strikethrough className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('bulletList')}
+          onPressedChange={() =>
+            editor.chain().focus().toggleBulletList().run()
+          }>
+          <List className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('orderedList')}
+          onPressedChange={() =>
+            editor.chain().focus().toggleOrderedList().run()
+          }>
+          <ListOrdered className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('blockquote')}
+          onPressedChange={() =>
+            editor.chain().focus().toggleBlockquote().run()
+          }>
+          <Quote className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('codeBlock')}
+          onPressedChange={() =>
+            editor.chain().focus().toggleCodeBlock().run()
+          }>
+          <Code className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('undo')}
+          onPressedChange={() => editor.chain().focus().undo().run()}>
+          <Undo className='h-4 w-4' />
+        </Toggle>
+        <Toggle
+          size='sm'
+          variant={'outline'}
+          pressed={editor.isActive('redo')}
+          onPressedChange={() => editor.chain().focus().redo().run()}>
+          <Redo className='h-4 w-4' />
+        </Toggle>
+      </div>
     </div>
   );
 };
